83291e937445c6d03ca475bb93d80458b50d0c4d
Heinrich <heinrichep@grabmayr.net>
2024-03-26 17:41:12 +0100

    CI

---
+++ picasso_workflow/tests/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+
+results_folder = os.abspath(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..", "..", "temp"
+    )
+)
+if not os.path.exists(results_folder):
+    os.mkdir(results_folder)
